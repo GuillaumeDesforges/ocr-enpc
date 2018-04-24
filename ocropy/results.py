e6d5b3c02fdb6af3c41b6a9496920ddb910791b0
Théo VIEL <theo.viel@eleves.enpc.fr>
2018-04-24 23:44:51 +0200

    datasets, cr 24/04

--- ocropy/results.py
+++ ocropy/results.py
@@ -6,15 +6,16 @@ import codecs
 import matplotlib.pyplot as plt
 
 
-pathref = "C:\\Users\\teovi\\Documents\\IMI\Projet ML\ocr-enpc\ocropy\Models\Default_normalized"
-labelref = "Non normalisé"
+pathref = "C:\\Users\\teovi\\Documents\\IMI\Projet ML\ocr-enpc\ocropy\Models\Default"
+labelref = "Learning rate = 0.001"
 os.chdir(pathref)
 
 dataref = codecs.open('results.txt', 'r', encoding='utf-8')
 indexref= codecs.open('index.txt', 'r', encoding='utf-8')
 
-path = "C:\\Users\\teovi\\Documents\\IMI\Projet ML\ocr-enpc\ocropy\Models\\Default_n_hiddensize=200"
-label = "Normalisé"
+# path = "C:\\Users\\teovi\\Documents\\IMI\Projet ML\ocr-enpc\ocropy\Models\\Default_n_hiddensize=200"
+label = "Learning rate = 0.0001"
+path = "C:\\Users\\teovi\\Documents\\ocropy"
 
 os.chdir(path)
 
@@ -27,10 +28,11 @@ indexesref=[]
 errorref=[]
 
 for line in index:
-    indexes.append(int(line[13:-10]))  #Mettre -10 à la place de -11 parfois
+    # print(line)
+    indexes.append(int(line[14:-10]))  #Mettre -10 à la place de -11 parfois
     
 for line in indexref:
-    indexesref.append(int(line[13:-11]))  #Mettre -10 à la place de -11 parfois
+    indexesref.append(int(line[13:-10]))  #Mettre -10 à la place de -11 parfois
     
 for line in data:
     if "err " in line:
@@ -41,14 +43,14 @@ for line in dataref:
         errorref.append(float(line[-11:-3]))
 
 print("L'erreur minimale est de "+ str(min(error)) +"%, atteinte pour " + str(indexes[np.argmin(error)]) + " itérations")
-print("L'erreur minimale est de "+ str(min(errorref)) +"%, atteinte pour " + str(indexesref[np.argmin(errorref)]) + " itérations pour la solution de référence")
+# print("L'erreur minimale est de "+ str(min(errorref)) +"%, atteinte pour " + str(indexesref[np.argmin(errorref)]) + " itérations pour la solution de référence")
 def plt_1courbe(indexes, error):
     plt.figure()
     line = plt.plot(indexes,error, linewidth=2)
     
     
     plt.ylim(0, max(error))
-    plt.xlim(min(indexes),105000)
+    plt.xlim(min(indexes),max(indexes)+1000)
     plt.legend(fontsize=30)
     plt.ylabel("Pourcentage d'erreurs sur les données de test", fontsize=30)
     plt.xlabel("Nombre d'itérations dans l'entraînement", fontsize=30)
@@ -82,7 +84,7 @@ def plt_2courbes(indexes1, error1, label1, indexes2, error2, label2):
     # plt.savefig('error_plot_compare.png')
 
     
-plt_1courbe(indexes, error)
-plt_2courbes(indexes, error, label,  indexesref, errorref, labelref)
+plt_1courbe(indexesref, errorref)
+# plt_2courbes([indexesref[10]]+ indexes, [errorref[10]] + error, label,  indexesref, errorref, labelref)
 index.close()
 data.close()
