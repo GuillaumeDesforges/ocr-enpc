c63e346473ac83f172c00058038ba95bde689e5b
Théo VIEL <theo.viel@eleves.enpc.fr>
2018-06-06 22:14:43 +0200

    LOT OF STUFF

--- ocropy/results.py
+++ ocropy/results.py
@@ -6,71 +6,90 @@ import codecs
 import matplotlib.pyplot as plt
 
 
-pathref = "C:\\Users\\teovi\\Documents\\IMI\Projet ML\ocr-enpc\ocropy\Models\Default"
-labelref = "Learning rate = 0.001"
-os.chdir(pathref)
+## First data
 
-dataref = codecs.open('results.txt', 'r', encoding='utf-8')
-indexref= codecs.open('index.txt', 'r', encoding='utf-8')
-
-# path = "C:\\Users\\teovi\\Documents\\IMI\Projet ML\ocr-enpc\ocropy\Models\\Default_n_hiddensize=200"
-label = "Learning rate = 0.00001"
-path = "C:\\Users\\teovi\\Documents\\ocropy"
+path = "C:\\Users\\teovi\\Documents\\IMI\\Projet Ocr\\Models\\hiddensize=500"
 
 os.chdir(path)
 
-index = codecs.open('index.txt', 'r', encoding='utf-8')
-data = codecs.open('results.txt', 'r', encoding='utf-8')
+label1 = "Learning rate = 0.0001"
 
 
-path2 = "C:\\Users\\teovi\\Documents\\IMI\\Projet ML\\Models\\adapt_lrate\\0.0001"
-label2 = "Learning rate = 0.0001"
-os.chdir(path2)
+data1 = codecs.open('results.txt', 'r', encoding='utf-8')
+index1 = codecs.open('index.txt', 'r', encoding='utf-8')
+
+
+indexes1 = []
+error1 = []
+
+for line in index1:
+    indexes1.append(int(line[13:-10]))  #Mettre -10 à la place de -11 parfois
+    
+for line in data1:
+    if "err " in line:
+        error1.append(float(line[-11:-3]))
+        
+print(min(error1))
+print(indexes1[np.argmin(error1)])
+
+## Second data
+
+path = "C:\\Users\\teovi\\Documents\\IMI\\Projet Ocr\\Models\\hiddensize=500\\train"
+os.chdir(path)
+
+label2 = "Learning rate = 0.00001"
+
 
 data2 = codecs.open('results.txt', 'r', encoding='utf-8')
-index2= codecs.open('index.txt', 'r', encoding='utf-8')
+index2 = codecs.open('index.txt', 'r', encoding='utf-8')
 
-indexes=[]
-error=[]
 
-indexesref=[]
-errorref=[]
-
-indexes2=[]
-error2=[]
-
-for line in index:
-    indexes.append(int(line[14:-10]))  #Mettre -10 à la place de -11 parfois
-    
-for line in indexref:
-    indexesref.append(int(line[13:-10]))  #Mettre -10 à la place de -11 parfois
+indexes2 = []
+error2 = []
 
 for line in index2:
     indexes2.append(int(line[13:-10]))  #Mettre -10 à la place de -11 parfois
     
-
-for line in data:
-    if "err " in line:
-        error.append(float(line[-11:-3]))
-
 for line in data2:
     if "err " in line:
         error2.append(float(line[-11:-3]))
 
-for line in dataref:
-    if "err " in line:
-        errorref.append(float(line[-11:-3]))
+print(min(error2))
+print(indexes2[np.argmin(error2)])
 
-# print(error2)
-# print(indexes2)
-print("L'erreur minimale est de "+ str(min(error)) +"%, atteinte pour " + str(indexes[np.argmin(error)]) + " itérations")
+## Third data
+
+path = "C:\\Users\\teovi\\Documents\\IMI\\Projet Ocr\\Models\\hiddensize=200\\0.00001"
+os.chdir(path)
+
+label3 = "Learning rate = 0.000001"
+
+
+data3 = codecs.open('results.txt', 'r', encoding='utf-8')
+index3 = codecs.open('index.txt', 'r', encoding='utf-8')
+
+
+indexes3 = []
+error3 = []
+
+for line in index3:
+    indexes3.append(int(line[13:-10]))  #Mettre -10 à la place de -11 parfois
+    
+for line in data3:
+    if "err " in line:
+        error3.append(float(line[-11:-3]))
+
+# print(min(error3))
+# print(indexes3[np.argmin(error3)])
+
+## Plot functions
 
 def plt_1courbe(indexes, error):
     plt.figure()
     line = plt.plot(indexes,error, linewidth=2)
     
     
-    plt.ylim(0, max(error))
+    plt.ylim(0, 60)
     plt.xlim(min(indexes),max(indexes)+1000)
     plt.legend(fontsize=30)
     plt.ylabel("Pourcentage d'erreurs sur les données de test", fontsize=30)
@@ -91,8 +110,8 @@ def plt_2courbes(indexes1, error1, label1, indexes2, error2, label2):
     line2 = plt.plot(indexes2,error2, label=label2, linewidth=2)
     legend2 = plt.legend(handles=line2, loc=1)
     
-    plt.ylim(0, max(max(error1),max(error2)))
-    # plt.xlim(min(min(indexes), min(indexes2)),105000)
+    plt.ylim(0, 60)
+    # plt.xlim(1000, 50000)
     plt.legend(fontsize=30)
     plt.ylabel("Pourcentage d'erreurs sur les données de test", fontsize=30)
     plt.xlabel("Nombre d'itérations dans l'entraînement", fontsize=30)
@@ -117,7 +136,7 @@ def plt_3courbes(indexes1, error1, label1, indexes2, error2, label2, indexes3, e
     legend3 = plt.legend(handles=line2, loc=1)
     
     plt.ylim(0, max(max(error1),max(error2), max(error3)))
-    plt.xlim(0,120000)
+    plt.xlim(0,150000)
     plt.legend(fontsize=30)
     plt.ylabel("Pourcentage d'erreurs sur les données de test", fontsize=30)
     plt.xlabel("Nombre d'itérations dans l'entraînement", fontsize=30)
@@ -127,13 +146,30 @@ def plt_3courbes(indexes1, error1, label1, indexes2, error2, label2, indexes3, e
     figManager.window.showMaximized()
     plt.show()
  
-    
-# plt_1courbe(indexesref, errorref)
-# plt_2courbes(indexes2, error2, label2,  indexesref[:49], errorref[:49], labelref)
-plt_3courbes(indexesref[:49], errorref[:49], labelref, indexes2[:78-48], error2[:78-48], label2, indexes, error, label)
-index.close()
-data.close()
+
+## Calls
+
+
+# plt_1courbe(indexes1, error1)
+# plt_2courbes(indexes1[:36], error1[:36], label1,  indexes2, error2, label2)
+# plt_3courbes(indexes1[:95], error1[:95], label1,  indexes2, error2, label2, indexes3, error3, label3 )
+
 
 # print(indexes)
 # print(indexes2)
-# print(indexesref)
+
+##
+
+X = [8.86, 7.55, 7.64]
+X2 = [2.90, 1.75, 1.6]
+Y = [100, 200, 500]
+Ylog = np.log(np.array([100, 200, 500]))
+plt.figure()
+plt.grid(True)
+plt.plot(Y,X, label = "Test data")
+plt.plot(Y,X2, label= "Train data")
+plt.title("Error in function of the size of the LSTM")
+plt.grid(True)
+plt.ylim(0,10)
+plt.legend()
+plt.show()
